dadd67115a5494af8455f3736b9021b124ec917c
Ankit Srivastava <70625232+srivastavaan@users.noreply.github.com>
2022-04-21 12:50:35 +0200

    Project setup

---
+++ src/main/java/no/posten/imiagreementsimport/ImiagreementsimportApplication.java
@@ -0,0 +1,14 @@
+package no.posten.imiagreementsimport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImiagreementsimportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImiagreementsimportApplication.class, args);
+		System.out.println("Asdasdasdasd");
+	}
+
+}
---
+++ src/test/java/no/posten/imiagreementsimport/ImiagreementsimportApplicationTests.java
@@ -0,0 +1,13 @@
+package no.posten.imiagreementsimport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImiagreementsimportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
